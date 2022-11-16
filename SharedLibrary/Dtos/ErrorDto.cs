dc44ff14e4965feb88d392ef465f503a200bddab
Anıl Orhan <anilorhanmail@gmail.com>
2022-11-16 23:58:22 +0300

    first commit

---
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class ErrorDto
+    {
+        public List<string> Errors { get; private set; }
+        public bool IsShow { get; private set; }
+
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+
+        }
+
+        public ErrorDto(string errors, bool isShow)
+        {
+            Errors.Add(errors);
+            this.IsShow = isShow;
+        }
+
+        public ErrorDto(List<string>errors,bool isShow)
+        {
+            Errors=errors;
+            IsShow=isShow;
+            
+        }
+    }
+}
